85428beeb5816c56ba477e79c7acfbb5f4334035
Jonas <jonas@uckermark.consulting>
2025-02-05 16:12:40 +0100

    rename utilitry post record

--- utils/API_POST_DNS_RECORDS.js
+++ utils/API_POST_DNS_RECORDS.js
@@ -2,17 +2,9 @@ const axios = require('axios');
 const API_KEY = `${process.env.IONOS_API_PREFIX}.${process.env.IONOS_API_SECRET}`;
 const API_ZONE_URL = `${process.env.DNS_BASE_URL}/zones`;
 
-async function createDnsRecords(domainId, domainName, type, content, ttl) {
+async function API_POST_DNS_RECORDS(domainId, domainName, type, content, ttl) {
     try {
         const response = await axios.post(`${API_ZONE_URL}/${domainId}/records`, 
-            {
-                name: subdomainname,
-                type: subdomaintype,
-                content: subdomainip,
-                ttl: subdomainttl,
-                prio: 0,
-                disabled: false
-            }, 
             {
                 headers: {
                     'accept': 'application/json',
@@ -29,4 +21,4 @@ async function createDnsRecords(domainId, domainName, type, content, ttl) {
     }
 }
 
-module.exports = { createDnsRecords };
+module.exports = { API_POST_DNS_RECORDS };
