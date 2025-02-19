57b70dba9a47791c70af09d8ecbcce812935a953
Jonas <jonas@uckermark.consulting>
2025-02-19 21:53:12 +0100

    nav bar helmet

---
+++ public/js/nav-bar.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef -- used for history back btns in dns views*/
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById('userDropdown').addEventListener('click', function(event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+  });
+});
