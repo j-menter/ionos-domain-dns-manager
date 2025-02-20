3d2472f6ee8465b2d03e42b74bbedba9f5db55de
Jonas <jonas@uckermark.consulting>
2025-02-20 20:52:43 +0100

    new migration

--- prisma/migrations/20250220195200_init/migration.sql
+++ prisma/migrations/20250220195200_init/migration.sql
@@ -1,17 +1,22 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `benutzername` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `passwordHash` VARCHAR(191) NOT NULL,
+    `isAdmin` BOOLEAN NOT NULL DEFAULT false,
 
-  - You are about to drop the column `userId` on the `domain` table. All the data in the column will be lost.
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-*/
--- DropForeignKey
-ALTER TABLE `domain` DROP FOREIGN KEY `Domain_userId_fkey`;
+-- CreateTable
+CREATE TABLE `Domain` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
 
--- DropIndex
-DROP INDEX `Domain_userId_fkey` ON `domain`;
-
--- AlterTable
-ALTER TABLE `domain` DROP COLUMN `userId`;
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `_UserDomains` (
